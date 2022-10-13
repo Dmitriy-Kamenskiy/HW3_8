7a936ebbe172bd2f7b4831e0dfd2ebcb51ca32ce
Dmitriy-Kamenskiy <webhd@yandex.ru>
2022-10-13 10:49:22 +0300

    Attraction with Queue commit

--- src/Main.java
+++ src/Main.java
@@ -1,25 +1,26 @@
 
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
 
 public class Main {
     public static void main(String[] args) {
         Person person;
-        List<Person> personList;
-        personList = generateClients();
-        while (!personList.isEmpty()){
-            person = personList.get(0);
+        Queue<Person> personQueue;
+        personQueue = (Queue<Person>) generateClients();
+        while (!personQueue.isEmpty()){
+            person = personQueue.peek();
             System.out.println(person.name + " " + person.lastName + " прокатился");
             person.used();
-            personList.remove(person);
+            personQueue.poll();
             if (person.ticketNumbers > 0) {
-                personList.add(person);
+                personQueue.offer(person);
             }
         }
     }
 
     public static List<Person> generateClients() {
-        List<Person> persons = new LinkedList<>();
+        List <Person> persons = new LinkedList<>();
         persons.add(new Person("Петя", "Иванов", 2));
         persons.add(new Person("Рулон", "Обоев", 3));
         persons.add(new Person("Алексей", "Нетоложко", 2));
